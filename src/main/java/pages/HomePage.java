2add165e93cd14b52b3c079599077356c57caf50
aehabe <ahmed.ehab@aqarmap.com>
2020-01-06 13:42:33 +0200

    test for single search and multi search

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,9 +1,11 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage extends PageBase  {
@@ -20,5 +22,22 @@ public class HomePage extends PageBase  {
 		wait.until(ExpectedConditions.elementToBeClickable(loginButton));
 		click(loginButton);
 	}
+	
+	@FindBy(xpath="//*[@id=\"search-box\"]/div[1]/div[2]/div[3]/span/span[1]/span/ul/li/input")
+	public WebElement searchbar;
+	@FindBy(id= "search_btn")
+public WebElement searchbtn;
+	//suggested search results
+	@FindBy(xpath="/html/body/div[4]/div[3]/div/div/div/div/div/form[1]/div[1]/div[2]/div[3]/span[2]/span/span/ul/li")
+public WebElement suggestloc1;
+	
+
+public void suggestsearches() {
+	
+	WebElement suggest = driver.findElement(By.id("select2-search_location-results"));
+	Select option = new Select(suggest);
+	option.selectByVisibleText("Abbas El Akkad");
+	
+}
 
 }
